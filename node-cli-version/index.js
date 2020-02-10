74e4873a571f41122e26098c12ce9cb6fa0d3529
Jean Menezes <jeanmenezes.nave@gmail.com>
2020-02-09 22:25:56 -0300

    Add basic structure for CLI version of Jokenpo

---
+++ node-cli-version/index.js
@@ -0,0 +1,68 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Welcome to my node.js version of Jokempo!");
+console.log("type which option you want to select");
+var playAgain = function() {
+    rl.question('Play again? (y/n) ', answer => {
+        const options = {
+            y: 1,
+            n: 2,
+        };
+        const userChoice = options[answer];
+        if(userChoice === 1) {
+            recursiveQuestion();
+        } else {
+            console.log("Bye!");
+            rl.close();
+        }
+    })
+}
+
+var recursiveQuestion = function() {
+    rl.question('Select your option: rock, paper or scissor? ', answer => {
+    const options = {
+        rock: 1,
+        paper: 2,
+        scissor: 3
+    };
+    const userChoice = options[answer];
+    const aiChoice = Math.round(Math.random() * 2) + 1;
+    
+    console.log("AI's choice: ", Object.keys(options).find(key => options[key] === aiChoice));
+    if(userChoice) {
+        if (userChoice === 1 && aiChoice === 3) {
+            console.log('You win!');
+        } else if (userChoice === 1 && aiChoice === 2) {
+            console.log('You lose!');
+        } else if (userChoice === 1 && aiChoice === 1) {
+            console.log('It\'s a tie!');
+        }
+
+        if (userChoice === 2 && aiChoice === 1) {
+            console.log('You win!');
+        } else if (userChoice === 2 && aiChoice === 3) {
+            console.log('You lose!');
+        } else if (userChoice === 2 && aiChoice === 2) {
+            console.log('It\'s a tie!');
+        }
+
+        if (userChoice === 3 && aiChoice === 2) {
+            console.log('You win!');
+        } else if (userChoice === 3 && aiChoice === 1) {
+            console.log('You lose!');
+        } else if (userChoice === 3 && aiChoice === 3) {
+            console.log('It\'s a tie!');
+        }
+        playAgain();
+    } else {
+        console.log("Please type a valid option");
+        recursiveQuestion();
+    }
+});
+}
+recursiveQuestion();
